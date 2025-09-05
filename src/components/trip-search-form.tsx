51b0a817b768e14ade7864fd51cefaa6840c757c
MoJiB Rsm <mojibrsm8989@gmail.com>
2025-09-05 12:58:36 +0000

    flight search form and hide pwerred by-
    <script async src="https://tpe

--- src/components/trip-search-form.tsx
+++ src/components/trip-search-form.tsx
@@ -29,7 +29,7 @@ const hotelFormSchema = z.object({
 const FlightWidget = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://tpembd.com/content?currency=bdt&trs=450435&shmarker=592431&show_hotels=true&powered_by=true&locale=en&searchUrl=www.aviasales.com%2Fsearch&primary_override=%2332a8dd&color_button=%2332a8dd&color_icons=%2332a8dd&dark=%23262626&light=%23FFFFFF&secondary=%23FFFFFF&special=%23C4C4C4&color_focused=%2332a8dd&border_radius=0&plain=false&promo_id=7879&campaign_id=100';
+    script.src = 'https://tpembd.com/content?currency=bdt&trs=450435&shmarker=592431&show_hotels=true&powered_by=true&locale=en&searchUrl=www.aviasales.com%2Fsearch&primary_override=%2332a8dd&color_button=%2332a8dd&color_icons=%2332a8dd&dark=%23262626&light=%23FFFFFF&secondary=%23FFFFFF&special=%23C4C4C4&color_focused=%2332a8dd&border_radius=0&no_labels=&plain=true&promo_id=7879&campaign_id=100';
     script.async = true;
     script.charset = 'utf-8';
 
